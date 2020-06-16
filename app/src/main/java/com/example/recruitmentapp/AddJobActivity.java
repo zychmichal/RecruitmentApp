757cc93f5c63f46ae2aaaa91f08bb46f97cf24dc
Zych <zychallo15@gmail.com>
2020-06-16 23:10:59 +0200

    Add back button

--- app/src/main/java/com/example/recruitmentapp/AddJobActivity.java
+++ app/src/main/java/com/example/recruitmentapp/AddJobActivity.java
@@ -1,10 +1,14 @@
 package com.example.recruitmentapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.media.session.PlaybackState;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +25,12 @@ public class AddJobActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         btn_create = findViewById(R.id.btn_create);
         et_companyName = findViewById(R.id.et_companyName);
@@ -56,5 +66,22 @@ public class AddJobActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. Use NavUtils to allow users
+            // to navigate up one level in the application structure. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            NavUtils.navigateUpTo(this, new Intent(this, ItemListActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
